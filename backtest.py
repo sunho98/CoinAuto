852aa9bdcdb24569858df5120d1b1b07108b2e58
sunho98 <168238894+sunho98@users.noreply.github.com>
2024-05-03 01:27:39 +0900

    Add files via upload

---
+++ backtest.py
@@ -0,0 +1,24 @@
+import pyupbit
+import numpy as np
+
+#변동성 돌파 전략
+#OHLCV(open, high, low, close, volume)로 당일 시가, 고가, 저가, 종가, 거래량에 대한 데이터
+df = pyupbit.get_ohlcv("KRW-BTC", count=7)
+
+
+# 변동폭 * k 계산,(고가 - 저가) *k값
+df['range'] = (df['high'] - df['low']) * 0.5
+#target(매수가), range 컬럼을 한칸씩 밑으로 내림(.shitft(1))
+df['target'] = df['open'] + df['range'].shift(1)
+#np.where(조건문, 참일 때 값, 거짓일 때 값)
+df['ror'] = np.where(df['high'] > df['target'],
+                     df['close'] / df['target'],
+                     1)
+#누적 곱 계산(cumprod) => 누적 수익률
+df['hpr'] = df['ror'].cumprod()
+#Draw Down 계산(누적 최대 값과 현재 hpr 차이/누적 최대값 *100)
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+#MDD 계산
+print("MDD(%): ", df['dd'].max())
+#엑셀 출력
+df.to_excel("dd.xlsx")
